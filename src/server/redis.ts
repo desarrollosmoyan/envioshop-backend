a82204ed2c5619d80ec6a43617ad9f7560626487
ArtuArtaza <71021164+ArtuArtaza@users.noreply.github.com>
2022-10-25 12:51:28 -0300

    Update redis.ts

--- src/server/redis.ts
+++ src/server/redis.ts
@@ -2,7 +2,7 @@ import { createClient } from "redis";
 
 export const connectRedis = async () => {
   const client = createClient({
-    url: "rediss://default:AVNS_ij75WaV6YFD7gCAV5yc@redisbd-do-user-12128300-0.b.db.ondigitalocean.com:25061",
+    url: `${process.env.REDIS_URL}`,
   });
   client.on("ready", () => console.log("Redis DB connected successfully"));
   client.on("error", (error) => console.log(error));
